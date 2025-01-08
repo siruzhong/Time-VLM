3c7cae00459bca4160e8e49dae8aa1294813bd2f
siruzhong <1412578784@qq.com>
2025-01-08 11:09:25 +0800

    delete useless scripts

--- scripts/long_term_forecast/TimeDiff_all.sh
+++
@@ -1,8 +0,0 @@
-./scripts/long_term_forecast/ETT_script/TimeDiff_ETTh1.sh
-./scripts/long_term_forecast/ETT_script/TimeDiff_ETTh2.sh
-./scripts/long_term_forecast/ETT_script/TimeDiff_ETTm1.sh
-./scripts/long_term_forecast/ETT_script/TimeDiff_ETTm2.sh
-./scripts/long_term_forecast/ECL_script/TimeDiff.sh
-./scripts/long_term_forecast/Exchange_script/TimeDiff.sh
-./scripts/long_term_forecast/Weather_script/TimeDiff.sh
-./scripts/long_term_forecast/Traffic_script/TimeDiff.sh
